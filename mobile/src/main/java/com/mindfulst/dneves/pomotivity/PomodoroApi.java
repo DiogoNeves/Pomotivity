6317a8dfb54e2c646fe394dd44e48fd1569bc67f
Diogo Neves <diogo.neves@gmail.com>
2014-07-24 14:59:38 +0100

    Fixed my pull request mess-up with stats

--- mobile/src/main/java/com/mindfulst/dneves/pomotivity/PomodoroApi.java
+++ mobile/src/main/java/com/mindfulst/dneves/pomotivity/PomodoroApi.java
@@ -1,6 +1,13 @@
 package com.mindfulst.dneves.pomotivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -12,14 +19,106 @@ import java.util.concurrent.atomic.AtomicReference;
  * Class responsible for communicating with the underlying pomodoro logic.
  * <p/>
  * This serves as a layer between the UI on the various devices and the logic.
+ * Notifications, sounds, UI and update of third party services (e.g. sharing) are outside the scope of the API.
+ * State, core logic and methods are the scope of this API.
+ * <p/>
  * P.s. Also a nice way for me to hack the whole thing here and move to the right place later ;)
  */
 public class PomodoroApi {
   public class AlreadyRunningException extends Exception {}
 
+  /**
+   * Class responsible for keeping simple stats state.
+   */
+  public static final class Stats {
+    public final int finishedToday;
+    public final int allTime;
+    public final int totalDays;
+
+    /**
+     * Default constructor.
+     */
+    protected Stats() {
+      finishedToday = 0;
+      allTime = 0;
+      totalDays = 0;
+    }
+
+    /**
+     * Attribute constructor.
+     *
+     * @param finishedToday Today's counter.
+     * @param allTime       All time pomodoro counter.
+     * @param totalDays     Total days with Pomodoros.
+     */
+    protected Stats(int finishedToday, int allTime, int totalDays) {
+      this.finishedToday = finishedToday;
+      this.allTime = allTime;
+      this.totalDays = totalDays;
+    }
+
+    /**
+     * Constructor that loads the initial values from the preferences.
+     *
+     * @param context     Context where to get the attribute keys from.
+     * @param preferences Preferences to load the attributes from.
+     */
+    protected Stats(Context context, SharedPreferences preferences) {
+      this.finishedToday = preferences.getInt(context.getString(R.string.finished_today_key), 0);
+      this.allTime = preferences.getInt(context.getString(R.string.all_time_key), 0);
+      this.totalDays = preferences.getInt(context.getString(R.string.total_days_key), 0);
+    }
+
+    /**
+     * Resets the today counter but keeps the other counters intact.
+     *
+     * @return new Stats instance with reset today counter.
+     */
+    protected Stats resetToday() {
+      return new Stats(0, allTime, totalDays);
+    }
+
+    /**
+     * Increments both finished today and all time.
+     *
+     * @return new Stats instance with the incremented counters.
+     */
+    protected Stats incrementCounter() {
+      return new Stats(finishedToday + 1, allTime + 1, totalDays);
+    }
+
+    /**
+     * Increments the day counter.
+     * This will reset the finishedToday value.
+     *
+     * @return new Stats instance with the information about the next day.
+     */
+    protected Stats nextDay() {
+      return new Stats(0, allTime, totalDays + 1);
+    }
+
+    @Override
+    public String toString() {
+      return String
+          .format("PomodoroApi.Stats(finishedToday:%d, allTime:%d, totalDays:%d)", finishedToday, allTime, totalDays);
+    }
+
+    /**
+     * Saves all the Stats attributes but doesn't call apply() or commit().
+     *
+     * @param context    Context where to get the attribute keys from.
+     * @param prefEditor Editor used to save the state.
+     */
+    protected void save(Context context, SharedPreferences.Editor prefEditor) {
+      prefEditor.putInt(context.getString(R.string.finished_today_key), finishedToday)
+                .putInt(context.getString(R.string.all_time_key), allTime)
+                .putInt(context.getString(R.string.total_days_key), totalDays);
+    }
+  }
+
   private static final String DEBUG_TAG         = "pomoapi";
   // 25 mins in seconds
-  private static final int    POMODORO_DURATION = 25;
+  private static final int    POMODORO_DURATION = 25 * 60;
 
   private static PomodoroApi mInstance = null;
 
@@ -29,6 +128,9 @@ public class PomodoroApi {
   private boolean mIsPaused  = false;
   private boolean mAutoStart = false;
 
+  private Stats    mStats            = new Stats();
+  private DateTime mLastPomodoroDate = new DateTime(0).withTime(4, 0, 0, 0);
+
   public static PomodoroApi getInstance() {
     if (mInstance == null) {
       mInstance = new PomodoroApi();
@@ -37,6 +139,42 @@ public class PomodoroApi {
   }
 
   private PomodoroApi() {}
+
+  public Stats getStats() {
+    return mStats;
+  }
+
+  public void save(Context context, SharedPreferences.Editor prefEditor) {
+    DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+    prefEditor.putString(context.getString(R.string.last_pomodoro_key), formatter.print(mLastPomodoroDate));
+    mStats.save(context, prefEditor);
+  }
+
+  /**
+   * Loads the state from the given preferences.
+   *
+   * @param context     Context where to get the attribute keys from.
+   * @param preferences Preferences to load the state from.
+   */
+  public void load(Context context, SharedPreferences preferences) {
+    mStats = new Stats(context, preferences);
+    DateTimeFormatter formatter = ISODateTimeFormat.dateTime();
+    final String defaultDate = formatter.print(new DateTime(0).withTime(4, 0, 0, 0));
+    String lastPomodoroStr = preferences.getString(context.getString(R.string.last_pomodoro_key), defaultDate);
+    try {
+      mLastPomodoroDate = formatter.parseDateTime(lastPomodoroStr);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    // We don't change the date because we only only to count another day when a pomodoro finishes
+    // This is just to avoid displaying today's pomodoros if a few days have passed since last running the app
+    DateTime now = DateTime.now().withTime(4, 0, 0, 0);
+    if (Days.daysBetween(now, mLastPomodoroDate).getDays() != 0) {
+      mStats = mStats.resetToday();
+    }
+  }
 
   /**
    * Starts the pomodoro timer.
@@ -65,10 +203,12 @@ public class PomodoroApi {
         if (mCurrentTime <= 0) {
           Log.d(DEBUG_TAG, "Timer ended after " + ((System.nanoTime() - mStartTime) * 1e-9));
           stop();
+          incrementStats();
           if (mAutoStart) {
             try {
               start();
-            } catch (AlreadyRunningException e) {
+            }
+            catch (AlreadyRunningException e) {
               Log.w(DEBUG_TAG, "It failed to auto-start because it was already running, but I just stopped...");
             }
           }
@@ -82,6 +222,19 @@ public class PomodoroApi {
     Log.i(DEBUG_TAG, "Time started");
     mIsPaused = false;
     mCurrentPomodoro.set(mExecutionService.scheduleAtFixedRate(pomodoroTick, 1, 1, TimeUnit.SECONDS));
+  }
+
+  private void incrementStats() {
+    // We consider the start of the day at 4am as this should be the least convenient time to use pomodoros
+    // see https://www.ted.com/talks/rives_on_4_a_m
+    DateTime now = DateTime.now().withTime(4, 0, 0, 0);
+    if (Days.daysBetween(now, mLastPomodoroDate).getDays() != 0) {
+      mStats = mStats.nextDay();
+      mLastPomodoroDate = now;
+    }
+    // Do this after the next day because it will reset the today counter
+    mStats = mStats.incrementCounter();
+    Log.d(DEBUG_TAG, "Current stats: " + mStats);
   }
 
   /**
