17990b706826e59a2a01d8e2e13937dbab4fb53c
Diogo Neves <diogo.neves@gmail.com>
2014-08-29 11:14:00 +0100

    Added more Stats tests

--- mobile/src/androidTest/java/com/mindfulst/dneves/pomotivity/api/StatsTest.java
+++ mobile/src/androidTest/java/com/mindfulst/dneves/pomotivity/api/StatsTest.java
@@ -7,6 +7,7 @@ import android.test.MoreAsserts;
 
 import com.mindfulst.dneves.pomotivity.R;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -33,9 +34,11 @@ public class StatsTest extends InstrumentationTestCase {
       dataMap.put(context.getString(R.string.all_time_key), 4);
       dataMap.put(context.getString(R.string.total_days_key), 2);
 
-      Set<String> projectSet = new HashSet<String>(1);
-      projectSet.add(String.format("%s,1", projectName));
-      dataMap.put(context.getString(R.string.projects_key), projectSet);
+      if (projectName != null && !projectName.isEmpty()) {
+        Set<String> projectSet = new HashSet<String>(1);
+        projectSet.add(String.format("%s,1", projectName));
+        dataMap.put(context.getString(R.string.projects_key), projectSet);
+      }
 
       return new MockSharedPreferences(dataMap);
     }
@@ -125,6 +128,9 @@ public class StatsTest extends InstrumentationTestCase {
     assertDefaultValues(stats);
   }
 
+  /**
+   * Tests if the constructor loads the values from the shared preferences.
+   */
   public void testLoadConstructorWithData() {
     final Context context = getInstrumentation().getTargetContext();
     final String projectName = "Test Project";
@@ -134,19 +140,108 @@ public class StatsTest extends InstrumentationTestCase {
     assertEquals(3, stats.finishedToday);
     assertEquals(4, stats.allTime);
     assertEquals(2, stats.totalDays);
-    assertTrue(stats.getProjects().size() == 1);
-    MoreAsserts.assertContentsInAnyOrder(stats.getProjects().keySet(), projectName);
-    assertEquals(1, (int) stats.getProjects().get(projectName));
+    assertProject(stats, projectName, 1);
+  }
+
+  /**
+   * Tests if resetting stats with default values returns the same values.
+   */
+  public void testResetTodayFromDefaultValues() {
+    Stats stats = new Stats();
+    stats.resetToday();
+    assertDefaultValues(stats);
+  }
+
+  /**
+   * Tests if resetting stats with some data only resets the today value.
+   */
+  public void testResetTodayWithData() {
+    final Context context = getInstrumentation().getTargetContext();
+    final String projectName = "Test Today";
+    SharedPreferences prefs = MockSharedPreferences.createWithTestData(context, projectName);
+
+    Stats stats = new Stats(context, prefs).resetToday();
+    assertEquals(0, stats.finishedToday);
+    assertEquals(4, stats.allTime);
+    assertEquals(2, stats.totalDays);
+    assertProject(stats, projectName, 1);
+  }
+
+  /**
+   * Tests adding a new project actually adds it and returns the same values for the other attributes.
+   */
+  public void testAddNewProject() {
+    // From default values
+    final String projectName = "New Project";
+    Stats stats = new Stats().addProject(projectName);
+    assertEquals(0, stats.finishedToday);
+    assertEquals(0, stats.allTime);
+    assertEquals(0, stats.totalDays);
+    assertProject(stats, projectName, 0);
+
+    // From data
+    final Context context = getInstrumentation().getTargetContext();
+    // Don't create with a project otherwise we wouldn't be testing a new project
+    SharedPreferences prefs = MockSharedPreferences.createWithTestData(context, null);
+    stats = new Stats(context, prefs).addProject(projectName);
+    assertEquals(3, stats.finishedToday);
+    assertEquals(4, stats.allTime);
+    assertEquals(2, stats.totalDays);
+    assertProject(stats, projectName, 0);
+  }
+
+  /**
+   * Tests adding a project that already exists won't change a thing.
+   * Doesn't test other attributes because we can infer the result from testAddNewProject.
+   */
+  public void testAddExistingProject() {
+    // From default values
+    final String projectName = "Existing Project";
+    Stats stats = new Stats().addProject(projectName).addProject(projectName);
+    assertProject(stats, projectName, 0);
+
+    // From data
+    final Context context = getInstrumentation().getTargetContext();
+    SharedPreferences prefs = MockSharedPreferences.createWithTestData(context, projectName);
+    // Only need to add once because it was already loaded with the preferences
+    stats = new Stats(context, prefs).addProject(projectName);
+    assertProject(stats, projectName, 1);
+  }
+
+  /**
+   * Tests if adding multiple different projects adds all of them.
+   */
+  public void testAddMultiProjects() {
+    final String projectOne = "Project One";
+    final String projectTwo = "Project Two";
+    Stats stats = new Stats().addProject(projectOne).addProject(projectTwo);
+
+    assertTrue(stats.getProjects().size() == 2);
+    MoreAsserts.assertEquals(stats.getProjects().keySet(), new HashSet<String>(Arrays.asList(projectOne, projectTwo)));
+    assertEquals(0, (int) stats.getProjects().get(projectOne));
+    assertEquals(0, (int) stats.getProjects().get(projectTwo));
+
+    // Add project one again, the result should be the same
+    stats = stats.addProject(projectOne);
+    assertTrue(stats.getProjects().size() == 2);
+    assertEquals(0, (int) stats.getProjects().get(projectOne));
   }
 
   /**
    * Tests against stats default values.
+   *
    * @param stats Stats object to test.
    */
-  private void assertDefaultValues(Stats stats) {
+  private void assertDefaultValues(final Stats stats) {
     assertEquals(0, stats.finishedToday);
     assertEquals(0, stats.allTime);
     assertEquals(0, stats.totalDays);
     MoreAsserts.assertEmpty(stats.getProjects());
+  }
+
+  private void assertProject(final Stats stats, final String projectName, final int value) {
+    assertTrue(stats.getProjects().size() == 1);
+    MoreAsserts.assertContentsInAnyOrder(stats.getProjects().keySet(), projectName);
+    assertEquals(value, (int) stats.getProjects().get(projectName));
   }
 }
